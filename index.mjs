c83b0b03229698e467bc632429fbc68bb07f182c
cheoklate <gcheok88@gmail.com>
2022-03-12 00:23:00 +0800

    test heroku commit 1.10

--- index.mjs
+++ index.mjs
@@ -8,16 +8,31 @@ const SALT = 'Hire an assassin to kill me please';
 
 const { Pool } = pg;
 
-let pgConnectionConfigs;
-pgConnectionConfigs = {
-	user: 'gcheok',
-	host: 'localhost',
-	database: 'Project2',
-	port: 5432,
-};
+// ...
 
+let pgConnectionConfigs;
+
+// test to see if the env var is set. Then we know we are in Heroku
+if (process.env.DATABASE_URL) {
+  // pg will take in the entire value and use it to connect
+  pgConnectionConfigs = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  };
+} else {
+  // this is the same value as before
+  pgConnectionConfigs = {
+    user: 'gcheok',
+    host: 'localhost',
+    database: 'Project2',
+    port: 5432,
+  };
+}
 const pool = new Pool(pgConnectionConfigs);
 
+// ...
 const app = express();
 const PORT = 3004;
 
